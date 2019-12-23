b40567ecaa8048f770121992789a5aa7809f8dd5
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2019-12-23 23:08:29 +0000

    Migrate from pi2c to system i2c. Create variable for common commandline option.

--- buttonshim/buttonshim.sh
+++ buttonshim/buttonshim.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-I2C=/home/mutoh/newi2c/pi2c
-DEV=/dev/iic1
+I2C=/usr/sbin/i2c
+DEV=/dev/iic0
 ADDR=0x3f
+COM_OPT="-m tr -f ${DEV} -a ${ADDR} -c 1 -v -b "
 
 outbin() {
     printf '\'`echo "ibase=16;obase=8 ;"$1 | bc `                          
 }
 
-outbin 1F | sudo ${I2C} -f ${DEV} -a ${ADDR} -d w -c 1 -v -b -o 0x3
-outbin 00 | sudo ${I2C} -f ${DEV} -a ${ADDR} -d w -c 1 -v -b -o 0x2
-outbin 00 | sudo ${I2C} -f ${DEV} -a ${ADDR} -d w -c 1 -v -b -o 0x1
+outbin 1F | sudo ${I2C} ${COM_OPT} -d w -o 0x3
+outbin 00 | sudo ${I2C} ${COM_OPT} -d w -o 0x2
+outbin 00 | sudo ${I2C} ${COM_OPT} -d w -o 0x1
 
 while true
 do
-    sudo ${I2C} -f ${DEV} -a ${ADDR} -d r -c 1
+    sudo ${I2C} ${COM_OPT} -d r -c 1
     sleep 1
 done
